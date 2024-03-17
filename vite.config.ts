22a39187723debd69af08a48bc17dda18a56baed
sijab <kaczorowskid97@gmail.com>
2024-03-17 14:18:58 +0100

    fix: prod fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
+import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()]
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    EnvironmentPlugin([
+      'VITE_DEV_API_URL',
+      'VITE_PROD_API_URL',
+      'VITE_ENVIRONMENT'
+    ])
+  ],
+  server: {
+    port: 5170
+  },
+  build: {
+    outDir: 'dist'
+  },
+  define: {
+    'process.env': process.env
+  },
+  publicDir: 'public'
 });
