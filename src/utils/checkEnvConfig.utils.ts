a75f682f860c1a46db8e80456f19b848048efbe3
sijab <kaczorowskid97@gmail.com>
2024-04-01 18:06:57 +0200

    refactor: utils refactor

--- src/utils/checkEnvConfig.utils.ts
+++ src/utils/checkEnvConfig.utils.ts
@@ -1,6 +1,7 @@
-export const checkEnvConfig = (...args: Array<string | undefined>): boolean =>
+export const checkEnvConfig = (...args: Array<string | undefined>): void => {
   args.some((arg) => {
     if (!arg || arg === '') {
       throw new Error('Invalid .env config');
     }
   });
+};
